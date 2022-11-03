3812b296607cd7924ba14acd2b4e84cc57b23916
Mathias Wakgari <104831041+mathiaswakgari@users.noreply.github.com>
2022-11-03 13:33:32 +0300

    Update login.php

--- login.php
+++ login.php
@@ -9,6 +9,7 @@
     <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
     <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 </head>
 <body>
 
@@ -18,6 +19,37 @@
         <label>Password
             <i class="fa-solid fa-lock"></i><input type="password" placeholder="Type your password" class="input" name="password"></label>
         <input type="submit" value="LOGIN" id="login-button">
+        <p>
+            <?php
+            session_start();
+            include("connection.php");
+            include("functions.php");
+
+            if ($_SERVER['REQUEST_METHOD'] == "POST"){
+                $user_name = $_POST["username"];
+                $password = $_POST['password'];
+
+                if (!empty($user_name) && !empty($password)){
+                    $query = "select * from users where user_name= '$user_name' and _password = '$password' limit 1";
+                    $result = mysqli_query($con, $query);
+                    if ($result && mysqli_num_rows($result) > 0){
+                        $user_data = mysqli_fetch_assoc($result);
+                        if ($user_data['_password'] === $password){
+                            $_SESSION['user_name'] = $user_data['user_name'];
+                            header("Location: home.php");
+                            die;
+                        }
+                    }
+                    else{
+                        echo ("Username or password incorrect.");
+                    }
+                }
+                else{
+                    echo ("Empty fields, Try again.");
+                }
+            }
+            ?>
+        </p>
     </form>
 
 </div>
@@ -25,30 +57,4 @@
 </body>
 </html>
 
-<?php
-session_start();
-include("connection.php");
-include("functions.php");
 
-if ($_SERVER['REQUEST_METHOD'] == "POST"){
-    $user_name = $_POST["username"];
-    $password = $_POST['password'];
-
-    if (!empty($user_name) && !empty($password)){
-        $query = "select * from users where user_name= '$user_name' and _password = '$password' limit 1";
-        $result = mysqli_query($con, $query);
-        if ($result && mysqli_num_rows($result) > 0){
-            $user_data = mysqli_fetch_assoc($result);
-            if ($user_data['_password'] === $password){
-                $_SESSION['user_name'] = $user_data['user_name'];
-                header("Location: home.php");
-                die;
-            }
-
-        }
-        else{
-            header("Location: error.php");
-        }
-    }
-}
-?>
